15b09a26bf4f3e68358b6d6dffafc960d1e8ec8c
kiparvez <ndp943296@gmail.com>
2024-03-30 20:53:31 +0600

    first commit

---
+++ appointment/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializers
+# Create your views here.
+
+
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = models.Appointment.objects.all()
+    serializer_class = serializers.AppointmentSerializer
+    
+
+    # custom query kortechi 
+    def get_queryset(self):
+        queryset = super().get_queryset() #  10 no line ke niye aslam / patient ke inherit  korlam 
+        patient_id = self.request.query_params.get('patient_id')
+        if patient_id:
+            queryset  = queryset.filter(patient_id=patient_id) 
+        return queryset
---
+++ service/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from . import models
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Service
+        fields = '__all__'
+        
